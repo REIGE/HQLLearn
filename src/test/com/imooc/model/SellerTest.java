4432fd9339bb610d3d4940a599ec13b8ae458492
REIGE <407601459@qq.com>
2017-06-30 21:49:02 +0800

    更新查询语句

--- src/test/com/imooc/model/SellerTest.java
+++ src/test/com/imooc/model/SellerTest.java
@@ -13,25 +13,25 @@ import com.imooc.util.HibernateSessionFactory;
 
 public class SellerTest {
 	private Session session = null;
-	
+
 	@Test
 	public void testSelectClauseSelf(){
 		String hql = " select new Seller(s.name,s.tel,s.address) from Seller s ";
 		Query query = session.createQuery(hql);
 		List<Seller> sellers = query.list();
-		
+
 		for(Seller seller : sellers){
 			System.out.println("name: "+seller.getName());
 			System.out.println("tel:"+seller.getTel());
 			System.out.println("address:"+seller.getAddress());
 		}
 	}
-	
+
 	@Test
 	public void testSelectClauseMap(){
 		String hql = " select new map(s.name,s.tel,s.address) from Seller s ";
 		Query query =session.createQuery(hql);
-		
+
 		List<Map> maps = query.list();
 		for(Map map : maps){
 			System.out.println("name:"+map.get("0"));
@@ -39,43 +39,43 @@ public class SellerTest {
 			System.out.println("address:"+map.get("2"));
 		}
 	}
-	
+
 	@Test
 	public void testSelectClauseList(){
 		String hql = " select new list(s.name,s.tel,s.address) from Seller s ";
-		
+
 		Query query = session.createQuery(hql);
 		List<List> lists = query.list();
-		
+
 		for(List list : lists){
 			System.out.println("name : "+list.get(0));
 			System.out.println("tel:"+list.get(1));
 			System.out.println("address:"+list.get(2));
 		}
 	}
-	
+
 	/*
 	 * 1.name 2.tel 3.address 4.star
 	 */
 	@Test
 	public void testSelectClauseObjectArray(){
 		String hql = " select s.name from Seller s ";
-		
+
 		Query query = session.createQuery(hql);
-		
+
 		List<Object> list = query.list();
-		
+
 		for(Object obj : list){
 			System.out.println("name:"+obj);
 		}
 	}
-	
+
 	@Test
 	public void testFromClause(){
 		String hql = " from Seller  s ";
-		
+
 		Query query = session.createQuery(hql);
-		
+
 		List<Seller> sellers = query.list();
 		for(Seller seller : sellers){
 			System.out.println("name:"+seller.getName());
